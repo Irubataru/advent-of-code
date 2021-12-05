ab704ab36634647f3af59671bb4164035e7e17e8
Jonas Rylund Glesaaen <jonas@glesaaen.com>
2021-12-05 21:42:36 +0100

    More setup tweaking and init script

---
+++ 2021/init_day.bash
@@ -0,0 +1,15 @@
+#!/bin/env bash
+
+day=$1
+dir="day$day"
+
+mkdir "$dir"
+cd "$dir"
+npx tsc --init
+ln -s ../prettierrc.json .prettierrc.json
+ln -s ../eslintrc.json .eslintrc.json
+
+cp ../package.json .
+npm i
+
+touch "${dir}.ts"
