84b3eec9300c6af000ee686531b88cb9748c1f2a
Jonas Rylund Glesaaen <jonas@glesaaen.com>
2021-12-05 19:08:48 +0100

    Tried to fix formatting day1

--- 2021/day1/day1.ts
+++ 2021/day1/day1.ts
@@ -1,23 +1,25 @@
-import * as fs from 'fs'
+/* eslint-disable require-jsdoc */
+/* eslint-disable camelcase */
+import * as fs from 'fs';
 
 function read_input(filename: string): number[] {
-  var lines = fs.readFileSync(filename, 'utf8').split(/\r?\n/).slice(0, -1);
-  return lines.map(function(line: string) { return parseInt(line); });
+  const lines = fs.readFileSync(filename, 'utf8').split(/\r?\n/).slice(0, -1);
+  return lines.map(function(line: string) {
+    return parseInt(line);
+  });
 }
 
 function count_depth_increases(depths: number[], window: number) {
+  let previous_depth = Number.MAX_VALUE;
 
-  var previous_depth = Number.MAX_VALUE;
-
-  var increases: number = 0;
-  for (var i = 0; i < depths.length - window + 1; i++) {
-
-    var current_depth = depths
-      .slice(i + 1, i + window)
-      .reduce(
-        function(a: number, b: number) {
-          return a + b;
-        }, depths[i]);
+  let increases: number = 0;
+  for (let i = 0; i < depths.length - window + 1; i++) {
+    const current_depth = depths
+        .slice(i + 1, i + window)
+        .reduce(
+            function(a: number, b: number) {
+              return a + b;
+            }, depths[i]);
 
     if (current_depth > previous_depth) {
       increases += 1;
@@ -29,7 +31,7 @@ function count_depth_increases(depths: number[], window: number) {
   return increases;
 }
 
-var depths = read_input('input.dat');
+const depths = read_input('input.dat');
 
 console.log(count_depth_increases(depths, 1));
 console.log(count_depth_increases(depths, 3));
